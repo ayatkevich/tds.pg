d726ad5e6e3b4dbd354445a5f5aba4d222da5936
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-10-11 02:39:50 +0700

    add capability to silent errors

--- src/spec.js
+++ src/spec.js
@@ -7,7 +7,7 @@ describe("tds.pg", () => {
   beforeAll(() => sql.file("src/tds-pg.sql"));
   afterAll(() => sql.end());
 
-  test("", async () => {
+  test("with errors", async () => {
     const X = new Program([new Trace("trace").step("@").step("x").step("y")]);
     const x = new Implementation(X)
       .transition("@", "x", async () => {
@@ -44,5 +44,53 @@ describe("tds.pg", () => {
     await expect(sql`
       update "test" set "state" = 'wrong'
     `).rejects.toThrow("tds_transition_check");
+
+    await sql`
+      update "test" set "state" = 'y'
+    `;
+  });
+
+  test("without errors", async () => {
+    const X = new Program([new Trace("trace").step("@").step("x").step("y")]);
+    const x = new Implementation(X)
+      .transition("@", "x", async () => {
+        return ["y"];
+      })
+      .transition("x", "y", async () => {
+        return ["@"];
+      });
+
+    await sql`
+      drop table if exists "test" cascade;
+      create table "test" (
+        "state" text
+      );
+    `.simple();
+
+    await sql`
+      select "tds_setup"(
+        "~table" => 'test',
+        "~column" => 'state',
+        "~states" => ${X.states},
+        "~transitions" => ${X.transitions},
+        "~noErrors" => true
+      )
+    `;
+
+    await expect(sql`
+      insert into "test" ("state") values ('wrong') returning *
+    `).resolves.toEqual([]);
+
+    await expect(sql`
+      insert into "test" ("state") values ('x') returning *
+    `).resolves.toEqual([{ state: "x" }]);
+
+    await expect(sql`
+      update "test" set "state" = 'wrong' returning *
+    `).resolves.toEqual([]);
+
+    await expect(sql`
+      update "test" set "state" = 'y' returning *
+    `).resolves.toEqual([{ state: "y" }]);
   });
 });
--- src/tds-pg.sql
+++ src/tds-pg.sql
@@ -2,18 +2,23 @@ create or replace function "tds_check_transition"() returns trigger as $$
   declare
     "~column" text = tg_argv[0];
     "~transitions" text[][] = tg_argv[1];
+    "~noErrors" boolean = tg_argv[2];
     "~old" jsonb = to_jsonb(old);
     "~new" jsonb = to_jsonb(new);
   begin
-    if (array["~old"->>"~column", "~new"->>"~column"] not in (
+    if array["~old"->>"~column", "~new"->>"~column"] not in (
       select
           array[
             case when "~transition"->>0 = '@' then null else "~transition"->>0 end,
             "~transition"->>1
           ]
         from jsonb_array_elements(to_jsonb("~transitions")) as "~transition"
-    )) then
-      raise exception 'tds_transition_check: incorrect transition from % to %', "~old"->>"~column", "~new"->>"~column";
+    ) then
+      if "~noErrors" then
+        return null;
+      else
+        raise exception 'tds_transition_check: incorrect transition from % to %', "~old"->>"~column", "~new"->>"~column";
+      end if;
     end if;
     return new;
   end
@@ -23,18 +28,20 @@ create or replace function "tds_setup"(
   "~table" text,
   "~column" text = 'state',
   "~states" text[] = array[]::text[],
-  "~transitions" text[][] = array[]::text[][]
+  "~transitions" text[][] = array[]::text[][],
+  "~noErrors" boolean = false
 ) returns void as $$
   begin
     execute format(
       $query$
         create trigger "tds_transition_check" before insert or update on %I
         for each row
-        execute procedure "tds_check_transition"(%L, %L)
+        execute procedure "tds_check_transition"(%L, %L, %L)
       $query$,
       "~table",
       "~column",
-      "~transitions"
+      "~transitions",
+      "~noErrors"
     );
   end
 $$ language plpgsql;

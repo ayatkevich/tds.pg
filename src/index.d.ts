732c4b086b86e68d0b31cb5786ae0391cdc05154
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-10-18 17:19:34 +0700

    add typings

---
+++ src/index.d.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import postgres from "postgres";
+import { AnyProgram, Implementation } from "tds.ts";
+
+export declare class Table<T extends AnyProgram> {
+  constructor(
+    sql: ReturnType<typeof postgres>,
+    options: { schema: string; table: string; column: string; program: T },
+  );
+
+  channel: string;
+  setup(options?: { noErrors: boolean }): Promise<void>;
+  testOutputs(): Promise<void>;
+  listen(
+    fn: (data: {
+      reference: Record<string, unknown>;
+      data: { from: string; to: string; record: Record<string, unknown> };
+    }) => Promise<void>,
+  ): Promise<() => Promise<void>>;
+  handle(implementation: Implementation<T>): Promise<() => Promise<void>>;
+}
